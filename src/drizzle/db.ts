c37230665f6b1ba4bac5989cf91c027bb0e0b215
akashk11 <akashvinod777@gmail.com>
2024-05-24 23:03:20 +0530

    drizzle setup partial completion

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,13 +1,7 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
-import postgres from 'postgres';
 import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as schema from './schema';
 
-const client = postgres(process.env.DATABASE_URL as string, { max: 1 });
-const db = drizzle(client);
-async function main() {
-  await migrate(db, { migrationsFolder: 'drizzle/migrations' });
-  await client.end();
-}
-
-main();
+const client = postgres(process.env.DATABASE_URL as string);
+export const db = drizzle(client, { schema, logger: true });
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { db } from 'src/drizzle/db';
+import { user } from 'src/drizzle/schema';
+import { NewUserType } from 'src/drizzle/type';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(createUserDto: NewUserType): Promise<NewUserType[]> {
+    try {
+      const data = db.insert(user).values(createUserDto).returning();
+      return data;
+    } catch (error) {
+      return error;
+    }
   }
 
   findAll() {
