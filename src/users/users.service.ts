d1a1c1d3ca1a25d70dcd2a0ce9e22e80f8ab3ce0
akashk11 <akashvinod777@gmail.com>
2024-05-24 23:21:44 +0530

    basic CRUD create,get all,get one completed in users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,19 +9,31 @@ import { NewUserType } from 'src/drizzle/type';
 export class UsersService {
   async create(createUserDto: NewUserType): Promise<NewUserType[]> {
     try {
-      const data = db.insert(user).values(createUserDto).returning();
+      const data = await db.insert(user).values(createUserDto).returning();
       return data;
     } catch (error) {
       return error;
     }
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    try {
+      const data = await db.query.user.findMany();
+      return data;
+    } catch (error) {
+      return error;
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    try {
+      const data = await db.query.user.findFirst({
+        where: (user, { eq }) => eq(user.id, id),
+      });
+      return data;
+    } catch (error) {
+      return error;
+    }
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
